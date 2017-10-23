aa9aa65cafa1ff6116a52cb7024ac78462ee322d
Vincent Bergeron <vincent_bergeron98@hotmail.com>
2017-10-23 09:32:14 -0400

    Ajout d'une méthode permettant de générer un titre dans le dashboard

--- lib/spree/data_visualisations/data_visualisation.rb
+++ lib/spree/data_visualisations/data_visualisation.rb
@@ -2,6 +2,14 @@ module Spree
   module DataVisualisations
     class DataVisualisation
 
+      def stats_title(title)
+        content_tag :div, class: "col-md-12" do
+          content_tag :div, class: "page-header" do
+            content_tag :h2, title, class: "text-center"
+          end
+        end
+      end
+
       def view_name
         "data_visualisation"
       end
