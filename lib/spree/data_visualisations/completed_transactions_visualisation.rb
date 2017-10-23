c25f57a8aebdf43d38f0caa969f54e1946bcc20f
Vincent Bergeron <vincent_bergeron98@hotmail.com>
2017-10-23 10:36:06 -0400

    Ajout de CompletedTransactionsVisualisation

---
+++ lib/spree/data_visualisations/completed_transactions_visualisation.rb
@@ -0,0 +1,37 @@
+module Spree
+  module DataVisualisations
+    class CompletedTransactionsVisualisation < DataVisualisation
+
+      def view_name
+        "completed_transactions_visualisation"
+      end
+
+      def prepare(options = {})
+        today = Date.today
+        from = Date.new(Date.today.year, 6, 30)
+        to = from + 1.day
+
+        if today < from
+          from = from - 1.year
+          to = to - 1.year
+        end
+
+        options = {filters: {from: from, to: to}, div_options:{id: div_id, style: style}}.merge(options)
+        locals = {}
+        locals[:bar_graph_name] = bar_graph_name
+        locals[:bar_graph_data] = data(options[:filters])
+        locals[:div_options] = options[:div_options]
+        locals[:from] = options[:filters][:from]
+        locals[:to] = options[:filters][:to]
+        locals
+
+      end
+
+
+      def data(filters={})
+
+      end
+
+    end
+  end
+end
--- lib/spree_dashboard.rb
+++ lib/spree_dashboard.rb
@@ -7,6 +7,7 @@ require 'spree/data_visualisations/pie_chart_visualisation.rb'
 require 'spree/data_visualisations/top_taxonomies_visualisation.rb'
 require 'spree/data_visualisations/total_visualisation.rb'
 require 'spree/data_visualisations/user_data_visualisation.rb'
+require 'spree/data_visualisations/completed_transactions_visualisation.rb'
 
 require 'spree/data_visualisations/growth_visualisation.rb'
 
