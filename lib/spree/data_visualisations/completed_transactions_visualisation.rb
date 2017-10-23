f5348c2c496c84b9f698a68f1e3fe946af27083f
Vincent Bergeron <vincent_bergeron98@hotmail.com>
2017-10-23 10:43:04 -0400

    CompletedTransactions

--- lib/spree/data_visualisations/completed_transactions_visualisation.rb
+++ lib/spree/data_visualisations/completed_transactions_visualisation.rb
@@ -6,6 +6,10 @@ module Spree
         "completed_transactions_visualisation"
       end
 
+      def graph_name
+        "Transactions complétées"
+      end
+
       def prepare(options = {})
         today = Date.today
         from = Date.new(Date.today.year, 6, 30)
@@ -18,8 +22,8 @@ module Spree
 
         options = {filters: {from: from, to: to}, div_options:{id: div_id, style: style}}.merge(options)
         locals = {}
-        locals[:bar_graph_name] = bar_graph_name
-        locals[:bar_graph_data] = data(options[:filters])
+        locals[:graph_name] = graph_name
+        locals[:graph_data] = data(options[:filters])
         locals[:div_options] = options[:div_options]
         locals[:from] = options[:filters][:from]
         locals[:to] = options[:filters][:to]
