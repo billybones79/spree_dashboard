33f39dee62ccd55690d881956a441d663cbd6e9c
Quantumphilisp <quantumphilisp@gmail.com>
2019-04-30 12:58:34 -0400

    changed route name to make it less likely to conflict part: 2

--- config/initializers/dash.rb
+++ config/initializers/dash.rb
@@ -1,10 +1,10 @@
 Spree::Backend::Config.configure do |config|
   config.menu_items << config.class::MenuItem.new(
-      [:admin_dashboard],
+      [:admin_dash],
       'copy',
       label: :dashboard,
       partial: 'spree/admin/shared/dashboard_main_menu',
-      url: '/admin/dashboard'
+      url: '/admin/dash'
   )
 
 
