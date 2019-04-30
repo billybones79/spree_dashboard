b979cd1003d7ff14e5a9da3f8c1cf614b030d973
Quantumphilisp <quantumphilisp@gmail.com>
2019-04-30 11:59:53 -0400

    changed route name to make it less likely to conflict

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Spree::Core::Engine.routes.draw do
   # Add your extension routes here
 
   namespace :admin do
-    get 'dashboard', :to => 'dashboards#index', :as => :dashboard
+    get 'dash', :to => 'dashboards#index', :as => :dashboard
   end
 end
